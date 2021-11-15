379e86f62c3ea448f3f904681e37648dd2d5e74c
CamiloOY <amiloc333@gmail.com>
2021-11-15 22:34:42 +0000

    Fixed Finish button on quiz creation page

--- pages/create.jsx
+++ pages/create.jsx
@@ -30,7 +30,7 @@ class Create extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			addingQuestions: false,
+			addingQuestions: true,
 			questionNum: 1,
 			quizTitle: ""
 		};
@@ -53,13 +53,19 @@ class Create extends React.Component {
 		}
 	}
 
-	async addQuestion(data, helpers) {
+	async addQuestion(data, helpers, final = false) {
 		console.log("in addquestion: ", data);
 		try {
 			helpers.setSubmitting(true);
 			const response = await authAxios.post("/question", {...data, quiz_id: this.state.quiz_id, question_position: this.state.questionNum});
-			helpers.resetForm();
-			this.setState(state => ({questionNum: state.questionNum + 1}));
+			if(final) {
+				// TODO: Make this redirect to quiz page
+				this.props.router.push("/");
+			}
+			else {
+				helpers.resetForm();
+				this.setState(state => ({questionNum: state.questionNum + 1}));
+			}
 		}
 		catch(error) {
 			console.log(error);
@@ -84,28 +90,30 @@ class Create extends React.Component {
 				</Head>
 				{this.state.addingQuestions ? (
 					<>
-						<div className="flex shadow-md flex-row p-3 justify-between items-center">
-							<div className="font-semibold text-lg">{this.state.quizTitle}: Question {this.state.questionNum}</div>
-							<Button>Finish</Button>
-						</div>
 						<Formik initialValues={questionInitialValues} validationSchema={questionSchema} onSubmit={this.addQuestion}>
-							{({isSubmitting, handleChange, values, setFieldValue}) => (
-								<Form className="">
-									<Field name="question" placeholder="Question" as="textarea" rows="1" wrap="hard" id="question" className="border border-grey-300 mt-1 px-3 py-2 rounded- text-3xl font-semibold block m-auto resize-none overflow-hidden focus:outline-none focus:ring-1 focus:ring-lightBlue-500 focus:border-lightBlue-500" onChange={e => this.handleTitleChange(e, handleChange)} required />
-									<FieldArray name="answers">
-										{helpers => {
-											console.log(values);
-											return (
-											<div className="">
-												{values.answers.map((answer, i) => (
-													<Field name={`answers[${i}]`} placeholder="Answer text" component={AnswerInput} id={`answers[${i}]`} key={i} removeSelf={() => helpers.remove(i)} setCorrect={() => setFieldValue("correct_answer", i)} correct={values.correct_answer === i} />
-												))}
-												<Button type="button" onClick={() => helpers.push("")} className="mt-4">Add answer</Button>
-											</div>
-										)}}
-									</FieldArray>
-									<div className="flex justify-end fixed bottom-2 right-2"><Button type="submit" disabled={isSubmitting}>Add another question</Button></div>
-								</Form>
+							{(props) => (
+								<>
+									<div className="flex shadow-md flex-row p-3 justify-between items-center">
+										<div className="font-semibold text-lg">{this.state.quizTitle}: Question {this.state.questionNum}</div>
+										<Button onClick={() => this.addQuestion(props.values, props, true)}>Finish</Button>
+									</div>
+									<Form className="">
+										<Field name="question" placeholder="Question" as="textarea" rows="1" wrap="hard" id="question" className="border border-grey-300 mt-1 px-3 py-2 rounded- text-3xl font-semibold block m-auto resize-none overflow-hidden focus:outline-none focus:ring-1 focus:ring-lightBlue-500 focus:border-lightBlue-500" onChange={e => this.handleTitleChange(e, props.handleChange)} required />
+										<FieldArray name="answers">
+											{helpers => {
+												console.log(props.values);
+												return (
+												<div className="">
+													{props.values.answers.map((answer, i) => (
+														<Field name={`answers[${i}]`} placeholder="Answer text" component={AnswerInput} id={`answers[${i}]`} key={i} removeSelf={() => helpers.remove(i)} setCorrect={() => props.setFieldValue("correct_answer", i)} correct={props.values.correct_answer === i} />
+													))}
+													<Button type="button" onClick={() => helpers.push("")} className="mt-4">Add answer</Button>
+												</div>
+											)}}
+										</FieldArray>
+										<div className="flex justify-end fixed bottom-2 right-2"><Button type="submit" disabled={props.isSubmitting}>Add another question</Button></div>
+									</Form>
+								</>
 							)}
 						</Formik>
 					</>
